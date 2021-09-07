e083a9095247cbfac1a2c925cebb650c02e97406
abdul ahad <abdul_ahad1996@live.com>
2021-09-07 20:02:30 +0500

    network layer added

---
+++ network_layer/lib/network/layers/network_connectivity.dart
@@ -0,0 +1,13 @@
+
+import 'package:connectivity/connectivity.dart';
+
+class NetworkConnectivity {
+  static Future<bool> get status async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.none) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
